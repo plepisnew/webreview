74a6f7396d64cd45308340a74868269faf90fc88
Christopher <gusandchbh@student.gu.se>
2022-09-25 11:22:41 +0200

    Add basic version of profile page(not responsive, no functionality) based on the Figma design

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,32 +1,43 @@
 
 <template>
-  <div class="page">
-    <b-container class="profileContainer">
-      <b-button variant="primary">Upload image</b-button>
-      <b-card> Hello {{username}} </b-card>
-      <b-card> Description: {{description}} </b-card>
-      <b-card> Profile picture src: {{profilePictureSrc}} </b-card>
-    </b-container>
+  <div class="page profile">
+    <div class="profile-container">
+      <h5 class="font-weight-bold">{{username}} </h5>
+      <b-img class="w-50" src="/images/profile_default.png" alt="Your profile image"></b-img>
+      <br>
+      <b-button variant="primary" size="sm" class="mt-2">Upload photo</b-button>
+    </div>
+    <div class="about-container">
+      <h5 class="font-weight-bold">About me</h5>
+      <b-form-textarea
+        id="textarea"
+        v-model="description"
+        class="w-50 rounded-top w-100 h-100"
+      >      {{description}}  </b-form-textarea>
+    </div>
   </div>
 </template>
 
 <script>
 import { Api } from '@/Api'
-
 export default {
   name: 'profile',
   data() {
     return {
-      username: '',
-      description: '',
+      username: 'USERNAME',
+      description: 'Write something about yourself...',
       profilePictureSrc: ''
     }
   },
   mounted() {
-    Api.get('/users/') // 632c6af0b81e8921942bcb41/
+    Api.get('/users/') // 632f32e8c4b6624870da28f3
       .then(response => {
-        this.username = response.data.username
-        this.description = response.data.description
+        if (this.username.length > 0) {
+          this.username = response.data.username
+        }
+        if (this.description.length > 0) {
+          this.description = response.data.description
+        }
         this.profilePictureSrc = response.data.profilePictureSrc
       })
       .catch(error => {
@@ -39,5 +50,28 @@ export default {
 </script>
 
 <style>
+.profile {
+  display: flex;
+}
+
+.about-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  top: 20%;
+  left: 35%;
+  height: 20vh;
+  width: 30vw;
+}
+
+.profile-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  top: 20%;
+  left: 15%;
+}
 
 </style>
